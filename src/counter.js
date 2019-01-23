2714a9f8650bc3861f109717190b06a336979012
fronto <post.shreibikus@gmail.com>
2019-01-23 21:27:37 +0300

    modified the removal of the counter

--- src/counter.js
+++ src/counter.js
@@ -4,12 +4,14 @@ import Button from '@material-ui/core/Button';
 export default class Counter extends React.Component {
 
 state = {
-  counter: 0
+  counter: this.props.value
 }
 
-incrementCount = () => {this.setState({counter: this.state.counter + 1})}
+incrementCount = () => this.setState({counter: this.state.counter + 1})
 
-decrementCount = () => {this.setState(this.state.counter > 0 ? {counter: this.state.counter - 1} : {counter: 0})}
+decrementCount = () => this.setState(this.state.counter > 0 ? {counter: this.state.counter - 1} : {counter: 0})
+
+delete = () => this.props.delete(this.props.id);
 
   render() {
     return (
@@ -17,6 +19,8 @@ decrementCount = () => {this.setState(this.state.counter > 0 ? {counter: this.st
         <Button color="primary" variant="contained" onClick={this.incrementCount}>+</Button>
         <Button>{this.state.counter}</Button>
         <Button color="primary" variant="contained" onClick={this.decrementCount}>-</Button>
+        <Button onClick={this.delete}>Удалить счетчик</Button>
+      
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,27 +3,32 @@ import ReactDOM from "react-dom";
 import Counter from './counter';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
+import randomstring from 'randomstring';
 
 class App extends React.Component {
   state = {
-    counter: 0
+    counters: []
   }
-  deleteCount = () => {this.setState(this.state.counter > 0 ? {counter: this.state.counter - 1} : {counter: 0})}
+  deleteCount = (id) => {
+    this.setState((prevState) => {
+      return {counters: prevState.counters.filter((item) => item.id !== id)}
+    });
+  }
 
-  addCount = () => {this.setState({counter: this.state.counter + 1})}
+  addCount = () => {
+    this.setState((prevState) => {
+      return {counters: [...prevState.counters, {id: randomstring.generate(), value: 0}]};
+    })
+  };
   
   render() {
     return (
       <div>
         {
-          [...new Array(this.state.counter)].map((_, index)=> {return <Counter key={index}/>})
+          this.state.counters.map((item)=> {return <Counter key={item.id} id={item.id} value={item.value} delete={this.deleteCount}/>})
         }
         <Fab color="primary" onClick={this.addCount}>
           <AddIcon />
-        </Fab>
-        <Fab color="secondary" onClick={this.deleteCount}>
-          <DeleteIcon />
         </Fab>
       </div>
     )
